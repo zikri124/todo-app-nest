06b71db50fde23696af86ce2f805e86ddc19d249
zikri124 <zikriaizet@gmail.com>
2023-12-30 17:29:47 +0700

    feat: create a interceptor for standarize the response from endpoint, apply it in auth service

---
+++ src/common/interceptors/response/response.interceptor.ts
@@ -0,0 +1,43 @@
+import { CallHandler, ExecutionContext, HttpException, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable, catchError, map, throwError } from 'rxjs';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((res: unknown) => this.responseHandler(res, context)),
+      catchError((err: HttpException) => throwError(() => this.errorHandler(err, context)))
+    )
+  }
+
+  errorHandler(exception: HttpException, context: ExecutionContext) {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      success: false,
+      statusCode: status,
+      path: request.url,
+      message: exception.message,
+      result: exception
+    })
+  }
+
+  responseHandler(res: any, context: ExecutionContext) {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const statusCode = response.statusCode;
+
+    return {
+      success: true,
+      path: request.url,
+      statusCode: statusCode,
+      result: res,
+    };
+  }
+}
